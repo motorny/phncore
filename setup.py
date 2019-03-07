50341b319032d0d11acb6c53bac21e76907bd4d7
motorny <motorny.nikita@gmail.com>
2019-03-08 00:23:35 +0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+packages = find_packages()
+
+
+def parse_reqs():
+    reqs_file = open('requirements.txt')
+    return [str(req_line) for req_line in reqs_file]
+
 
 setup(
     name='phncore',
     version='0.0.1',
-    packages=[''],
+    packages=packages,
     url='https://github.com/motorny/phncore',
     license='MIT',
     author='Motornyi Nikita',
     author_email='motorny.nikita@gmail.com',
-    description='PHNode (Personal House Node) core package'
+    description='PHNode (Personal House Node) core package',
+    install_requires=parse_reqs(),
 )
