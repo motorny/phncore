dc9821ee7d15f99493cfb873cfbc8e01b9c79ae1
motorny <motorny.nikita@gmail.com>
2019-03-29 23:18:11 +0300

    Added support for passing python path to a setup_env.bat script

--- utils/setup_env.bat
+++ utils/setup_env.bat
@@ -2,10 +2,15 @@
 rem Script for setting python venv for PHNcore and installing its package in develop mode
 rem Author: Nikita Motornyi
 
+echo Usage: Path to the python can be passed as an argument, if python is not in PATH variable
+
 rem Next variables will be local
 SETLOCAL
 
-rem Name of created environment
+rem Check whether path to a python is passed and set a corresponding variable (do not forget to handle a backslash after the path)
+IF "%~1"=="" GOTO NO_PARAMS
+set ARG_PY_DIR=%1\
+:NO_PARAMS
 
 rem Go to root repository directory
 cd .\..\
@@ -13,12 +18,13 @@ cd .\..\
 rem save it
 set ROOT_DIR=%CD%
 
+rem Name of created environment
 set ENV_NAME=env_core_37
 set ENV_DIR= %ROOT_DIR%\env\%ENV_NAME%
 echo Setting environment %ENV_NAME%
 
 rem setup env
-python -m venv %ENV_DIR%
+%ARG_PY_DIR%python -m venv %ENV_DIR%
 
 rem go to env folder and install phncore
 cd %ENV_DIR%\Scripts
