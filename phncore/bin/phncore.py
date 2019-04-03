5f67b30957ab529bdcec5e332b437de2dcd21c68
motorny <motorny.nikita@gmail.com>
2019-04-03 23:56:51 +0300

    Improved phncore.py manager for not to break up non-'nt' systems

--- phncore/bin/phncore.py
+++ phncore/bin/phncore.py
@@ -6,6 +6,8 @@ import argparse
 # expected to be exposed
 APP_SCRIPT_NAME = 'core.py'
 
+DETACHED_PROCESS = 0x00000008  # present in subprocess module only from python 3.7. Manually defined for compatibility
+
 
 def get_arguments():
     parser = argparse.ArgumentParser(description='Manager for PHNcore app.')
@@ -17,10 +19,15 @@ def get_arguments():
 def start_app():
     # provide a full path to the script (assume that it is exposed to the same directory, as a curent interpreter)
     # a script can not be called without 'python' on Windows, but a call with 'python' prefix only
-    # searches for in a current directory
+    # searches for scripts in a current directory
     script_path = os.path.join(os.path.dirname(sys.executable), APP_SCRIPT_NAME)
+
+    creationflags = None
+    if os.name == 'nt':
+        creationflags = DETACHED_PROCESS
+
     pid = subprocess.Popen(['python', script_path],
-                           creationflags=subprocess.DETACHED_PROCESS | subprocess.CREATE_NEW_PROCESS_GROUP).pid
+                           creationflags=creationflags).pid
 
     return pid
 
