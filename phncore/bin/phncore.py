5b396cd7c80cca03ef75096ade48f8ac3a4aa26c
motorny <motorny.nikita@gmail.com>
2019-04-14 20:34:57 +0300

    Added writing instance info file on daemon start

--- phncore/bin/phncore.py
+++ phncore/bin/phncore.py
@@ -2,9 +2,12 @@ import sys
 import os
 import subprocess
 import argparse
+import time
+import json
 
 # expected to be exposed
 APP_SCRIPT_NAME = 'core.py'
+DEFAULT_PORT = 8200
 
 
 def start_daemon(args):
@@ -22,10 +25,21 @@ def start_daemon(args):
                            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL).pid
     return pid
 
+def make_instance_record(pid, info):
+    instances_dir = os.path.join(sys.prefix, 'tmp', 'instances')
+    os.makedirs(instances_dir, exist_ok=True)
+
+    cur_timestamp = int(time.time())
+
+    instance_filename = '{0}_{1}.txt'.format(cur_timestamp, pid)
+    with open(os.path.join(instances_dir,instance_filename),'w') as f:
+        json.dump(info,f)
+
 
 def get_arguments():
     parser = argparse.ArgumentParser(description='Manager for PHNcore app.')
     parser.add_argument('--start', action='store_true', help='Start a new instance of an app')
+    parser.add_argument('--stop', action='store_true', help='Stop the most recent instance')
 
     return parser.parse_args()
 
@@ -38,6 +52,7 @@ def start_app():
     script_path = os.path.join(os.path.dirname(sys.executable), APP_SCRIPT_NAME)
 
     pid = start_daemon(['python', script_path])
+    make_instance_record(pid, {'port': DEFAULT_PORT})
     return pid
 
 
